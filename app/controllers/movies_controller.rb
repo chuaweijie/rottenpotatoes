6e96c581960fdd3a57885e8ddbe8f26f0a7fb55c
vplayer8 <chuawei_jie@hotmail.com>
2012-10-20 15:09:55 +0800

    Added checkboxes in views. Added code in controller to show checked ones.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+		@all_ratings = Movie.select("rating").group("rating")
+    @movies = Movie.where(:rating=>params[:ratings].keys)
   end
 
   def new
